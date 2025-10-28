f3e09a4373983bbfe19d54835acc3d15d802788a
Vasil <vasil.s.vasilev@abv.bg>
2025-10-28 17:18:03 +0200

    28.10 work

--- recipeTest/profiles/urls.py
+++ recipeTest/profiles/urls.py
@@ -3,4 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('create/', views.CreateProfileView.as_view(), name='create-profile'),
+    path('details/', views.DetailsProfileView.as_view(), name='details-profile'),
 ]
--- recipeTest/profiles/views.py
+++ recipeTest/profiles/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from profiles.models import Profile
 from django.urls import reverse_lazy
 from profiles.forms import ProfileCreateForm
+from recipeTest.utils import get_user_object
 # Create your views here.
 
 class CreateProfileView(CreateView):
@@ -10,3 +11,22 @@ class CreateProfileView(CreateView):
     form_class = ProfileCreateForm
     template_name = 'profiles/create-profile.html'
     success_url = reverse_lazy('home-page') # to cataloque-page
+
+
+class DetailsProfileView(DetailView):
+    template_name = 'profiles/details-profile.html'
+    context_object_name = 'profile'
+
+    def get_object(self, queryset=None):
+        return get_user_object()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Only published posts by this author
+        profile = self.get_object()
+        recipes = profile.recipes.all()
+        context['recipes_count'] = recipes.count()
+        return context
+
+
