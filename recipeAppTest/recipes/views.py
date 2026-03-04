d069613e997ac6b29c09ef704c161963c2e9fe02
Vasil <vasil.s.vasilev@abv.bg>
2026-03-04 07:00:51 +0200

    04.02 home

--- recipeAppTest/recipes/views.py
+++ recipeAppTest/recipes/views.py
@@ -21,6 +21,9 @@ class RecipeCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['profile'] = get_user_object()
+        # form = self.get_form()
+        # print(form.fields)  # test
+        # print(context['form'])  # test
         return context
 
 class CataloqueView(ListView):
