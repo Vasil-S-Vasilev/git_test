0e0c70342a04c87bee2b7fee9873ccdf220013ad
Vasil <vasil.s.vasilev@abv.bg>
2026-04-01 16:51:26 +0300

    01.04 work

--- libraryApi/books/urls.py
+++ libraryApi/books/urls.py
@@ -3,5 +3,6 @@ from books import views
 
 
 urlpatterns = [
-    path('books/', views.list_books_view, name = 'index')
+    path('books/', views.ListBookView.as_view(), name = 'books_list'),
+    # path('books/', views.list_books_view, name = 'index')
 ]
--- libraryApi/books/views.py
+++ libraryApi/books/views.py
@@ -5,6 +5,8 @@ from books.models import Book
 from books.serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 
 
 @api_view(['GET' ,'POST'])
@@ -13,7 +15,7 @@ def list_books_view(request):
         books = Book.objects.all()
 
         serializer = BookSerializer(books, many=True)
-
+        
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'POST':
@@ -25,3 +27,28 @@ def list_books_view(request):
     
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ListBookView(APIView):  # APIView is the base class, same as View in CBV
+
+    def get(self, request):
+        if request.method == 'GET':
+            books = Book.objects.all()
+
+            serializer = BookSerializer(books, many=True)
+
+            return Response(serializer.data, status=status.HTTP_200_OK) 
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# with Generic Api View
+class ListBooksApiView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
